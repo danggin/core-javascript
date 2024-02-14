e6f954d6fabaf74eeebf401d7f585176c3b57ba3
dain <danggin.min@gmail.com>
2024-02-15 01:16:52 +0900

    JS Random Dice

--- client/main.js
+++ client/main.js
@@ -1,3 +1,70 @@
-console.log("안녕 자바스크립트 !");
+import {
+    getNode,
+    getNodes,
+    insertLast,
+    diceAnimation,
+    clearContents,
+} from './lib/index.js';
 
-alert("안돼!");
+const [rollingButton, recordButton, resetButton] = getNodes(
+    '.buttonGroup > button'
+);
+const recordListWrapper = getNode('.recordListWrapper');
+
+const handleRollingDice = (() => {
+    let isClicked = false;
+    let stopAnimation;
+
+    return () => {
+        if (!isClicked) {
+            // 실행
+            stopAnimation = setInterval(diceAnimation, 100);
+            recordButton.disabled = true;
+            resetButton.disabled = true;
+        } else {
+            // 정지
+            clearInterval(stopAnimation);
+            recordButton.disabled = false;
+            resetButton.disabled = false;
+        }
+
+        isClicked = !isClicked;
+    };
+})();
+
+let count = 0;
+let total = 0;
+
+function renderRecordItem() {
+    const cube = getNode('#cube');
+    const diceValue = +cube.dataset.dice;
+    const record = getNode('.recordList > tbody');
+    const template = `
+    <tr>
+        <td>${++count}</td>
+        <td>${diceValue}</td>
+        <td>${(total += diceValue)}</td>
+    </tr>
+    `;
+
+    insertLast(record, template);
+}
+
+function handleRecord() {
+    recordListWrapper.hidden = false;
+
+    renderRecordItem();
+}
+
+function handleReset() {
+    recordListWrapper.hidden = true;
+
+    clearContents(getNode('tbody'));
+
+    count = 0;
+    total = 0;
+}
+
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
